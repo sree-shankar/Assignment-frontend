74852f5059bb57aa716738f57393c9bd01be9df7
sree-shankar <mmsreeshankar@gmail.com>
2025-06-18 21:13:55 +0530

    updated

--- src/data/apiData.js
+++ src/data/apiData.js
@@ -8,9 +8,9 @@ export const userProfile = {
 };
 
 export const apiUsage = [
-  { name: "File Upload API", method: "POST", time: "Jun 18, 03:50 PM", status: "Error", code: 500, responseTime: 600 },
-  { name: "Login API", method: "POST", time: "Jun 18, 02:44 PM", status: "Success", code: 200, responseTime: 350 },
-  { name: "Get User", method: "GET", time: "Jun 18, 01:30 PM", status: "Success", code: 200, responseTime: 280 },
-  { name: "Update API", method: "PATCH", time: "Jun 18, 12:00 PM", status: "Warning", code: 300, responseTime: 490 },
-  { name: "Delete User", method: "DELETE", time: "Jun 18, 11:00 AM", status: "Error", code: 500, responseTime: 450 }
+  { name: "File Upload API", method: "POST", time: " 03:50 PM", status: "Error", code: 500, responseTime: 600 },
+  { name: "Login API", method: "POST", time: " 02:44 PM", status: "Success", code: 200, responseTime: 350 },
+  { name: "Get User", method: "GET", time: " 01:30 PM", status: "Success", code: 200, responseTime: 280 },
+  { name: "Update API", method: "PATCH", time: "12:00 PM", status: "Warning", code: 300, responseTime: 490 },
+  { name: "Delete User", method: "DELETE", time: " 11:00 AM", status: "Error", code: 500, responseTime: 450 }
 ];
